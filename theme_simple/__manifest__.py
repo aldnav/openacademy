795424dde02daf326397b5129df2c06d561c745d
Aldrin Navarro <aldrin@newlogic.io>
2021-01-08 18:19:49 +0800

    Theme tutorial pt 1

---
+++ theme_simple/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+  'name':'Simple theme',
+  'description': 'Tutorial theme',
+  'version':'1.0',
+  'author':'Aldrin Navarro',
+  'data': [
+    'views/layout.xml',
+    'views/pages.xml',
+  ],
+  'category': 'Theme/Creative',
+  'depends': ['website'],
+  'application': False,
+  'installable': True,
+  'auto_install': False,
+}
